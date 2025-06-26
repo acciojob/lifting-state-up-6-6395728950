acffbe6873c4fb1d9008185b18d1732597810cf8
kuldeepparmar <kuldeep07122002@gmail.com>
2025-06-26 15:47:17 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import TodoList from "./TodoList";
 
 const App = () => {
+  const [task,settask] = useState([
+    {id:1,title:"Learn React",completed:false},
+    {id:2,title:"Build a React app",completed:false},
+    {id:3,title:"Deploy the React app",completed:false}
+    
+  ]);
+  
+ 
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <h2>Child Component</h2>
+      
+        
+       
+         
+          <TodoList task={task} settask={settask}/>
+    
+  
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,30 @@
+import React from "react";
+const TodoList=(props)=>{
+   const task = props.task;
+   const settask = props.settask;
+   const handleComplete = (id)=>{
+      const updatedtask = task.map(task=>task.id===id ?{...task,completed:true}:task);
+      settask(updatedtask);
+   };
+     
+    return (
+        
+        <div>
+         <ul>
+            {
+                task.map((item)=>(
+                <li key={item.id}>{item.title}
+                {
+                    !item.completed &&(<button onClick={()=>handleComplete(item.id)}>Complete</button>)
+                }
+                </li>
+                
+                ))
+            }
+         </ul>
+        </div>
+    )
+}
+
+
+export default TodoList;
